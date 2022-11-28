c52c3840c46b927325a94866016a9b540ba2bc65
Ryan Scott <rscott.sites@gmail.com>
2022-11-28 11:11:49 -0800

    First Commit

---
+++ Devote/DevoteApp.swift
@@ -0,0 +1,20 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by Ryan Scott on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
